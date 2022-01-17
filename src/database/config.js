bfdeefa0c0fd8f3672d6971c64738f40466adeb0
marco cruz <marco24cruz08@gmail.com>
2022-01-17 14:26:33 -0600

    first commit

---
+++ src/database/config.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION);
+    console.log('DB online');
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error starting DB');
+  }
+}
+
+module.exports = dbConnection;
